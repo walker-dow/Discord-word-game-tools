b4f1357319b287365ebdea47ce80ca8c10bc3444
walker-dow <36524784+walker-dow@users.noreply.github.com>
2022-04-09 22:12:21 -0700

    Update discord-hangman.py
    Added little pausey bit for Windows users

--- discord-hangman.py
+++ discord-hangman.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
-  
+
+import os
+
 def makeBlanks(word):
     wordBlanks = ""
     for i in word:
@@ -16,3 +18,7 @@ for word in phrase:
         hangmanPhrase += "   "
 
 print(hangmanPhrase)
+
+# Pause if we are running in Windows, to allow users to run the script easily from their file manager or desktop
+if os.name == "nt":
+    os.system('pause')
